cb5492fe557738290efee556430d3e509b0d4f5c
Huzefa Mohammed Hussain <huzefamhd@gmail.com>
2020-08-10 10:57:35 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby" 
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div style={{color: 'purple'}}>
+    <Link to="/contact/">Contacts</Link>
+    <Header headerText="Home page" />
+
+    <h1>Hello huzefa</h1>
+    <p>The first gatsby para</p> 
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  </div>
 }
