1e1a92f88c2a90082eb49bd36eccb04f9cd9b4d7
Huzefa Mohammed Hussain <huzefamhd@gmail.com>
2020-08-15 18:57:07 +0530

    added css and layouts

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,41 @@
 import React from "react"
-import Header from "../components/header"
+//import Header from "../components/header"
+import Layout from "../components/layout"
+import styles from "./about.module.css"
+
+console.log(styles)
+
+const User = props => (
+  <div className={styles.user}>
+    <img src={props.avatar} classname={styles.avatar} alt="" />
+    <div classname={styles.description}>
+    <h2 classname={styles.username}>{props.username}</h2>
+    <p classname={styles.excerpt}>{props.excerpt}</p>
+    </div>
+  </div>
+)
 
 export default function About() {
   return (
-    <div style={{ color: `teal` }}>
-      <Header headerText='About Gatsby'/>
-      <p>Such wow. Very React.</p>
-    </div>
+    <Layout>
+    
+      <h1>About css modules</h1>
+      <p>Css Modules are cool</p>
+    
+
+      <User 
+        username="Jane Foster"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+
+      <User
+        username="Bob Smith"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="I'm Bob Smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      
+    
+    </Layout>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,14 @@
 import React from "react"
-import Header from "../components/header"
-import { Link } from "gatsby" 
+import Layout from "../components/layout"
 
 export default function Home() {
-  return <div style={{color: 'purple'}}>
-    <Link to="/contact/">Contacts</Link>
-    <Header headerText="Home page" />
-
-    <h1>Hello huzefa</h1>
-    <p>The first gatsby para</p> 
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
-  </div>
+  return (
+    <Layout>
+      <h1>Hi! I'm building a fake Gatsby site as part of a tutorial!</h1>
+      <p>
+        What do I like to do? Lots of course but definitely enjoy building
+        websites.
+      </p>
+    </Layout>
+  )
 }
